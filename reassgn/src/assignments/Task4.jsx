f848c33e07e52df1ec656817d545c963b6d26c7f
nirmal199930 <nirmalchundru@gmail.com>
2024-11-19 13:02:11 +0530

    practice

--- reassgn/src/assignments/Task4.jsx
+++ reassgn/src/assignments/Task4.jsx
@@ -1,88 +1,54 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react'
 
 function Task4() {
-    const [circles, setCircles] = useState([]);
-
-    // Create circle function
-    const createCircle = useCallback(() => {
-        setCircles(prevCircles => [
-            ...prevCircles,
-            { isSelected: false, id: Math.trunc(Math.random() * 10000) },
-        ]);
-    }, [])
-
-    // Toggle circle selection
-    const makeSelected = useCallback((circleId) => {
-        setCircles(prevCircles =>
-            prevCircles.map(circle =>
-                circle.id === circleId
-                    ? { ...circle, isSelected: !circle.isSelected }
-                    : circle
-            )
-        );
-    }, []);
-
-    // Delete selected circles
-    const deleteCircle = useCallback(() => {
-        setCircles(prevCircles => prevCircles.filter(circle => !circle.isSelected));
-    }, []);
-
-    // Compute circle stats once
-    const totalCircles = circles.length;
-    const selectedCircles = circles.filter(circle => circle.isSelected).length;
-
+    const [circles, setCircles] = useState([])
+    function createCircle() {
+        let obj = {
+            isSelected: false,
+            id: Math.trunc(Math.random() * 10000)
+        }
+        setCircles([...circles, obj])
+    }
+    function makeSelected(circleId) {
+        // console.log(circleId);
+        let newCircles = circles.map((ele) => {
+            if (ele.id === circleId) {
+                ele.isSelected = !ele.isSelected
+            }
+            return ele
+        })
+        setCircles(newCircles)
+    }
+    function deleteCircle() {
+        let filterdCircles = circles.filter(ele => !ele.isSelected)
+        setCircles(filterdCircles)
+    }
     return (
         <>
-            <div style={{ marginTop: "100px" }}>
-                <button
-                    onClick={createCircle}
-                    style={{
-                        background: "green",
-                        marginLeft: "450px",
-                        height: "50px",
-                        color: "white",
-                        cursor: 'pointer'
-                    }}
-                >
-                    Create Circle
-                </button>
-                <button
-                    onClick={deleteCircle}
-                    style={{
-                        background: "red",
-                        height: "50px",
-                        marginLeft: "100px",
-                        color: "white",
-                        cursor: 'pointer'
-                    }}
-                >
-                    Delete Circle
-                </button>
+            <div>
+                <button onClick={createCircle} style={{background:"green"}}>Create Circle</button>
+                <button onClick={deleteCircle} style={{background:"red"}}>Delete Circle</button>
             </div>
-
-            <div style={{ marginLeft: "500px" }}>
-                <h3>No of circles: {totalCircles}</h3>
-                <h3>No of selected circles: {selectedCircles}</h3>
+            <div>
+                <h3>No of circles :{circles.length}</h3>
+                <h3>No of selected circles :{circles.filter(ele => ele.isSelected).length}</h3>
             </div>
-
             <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-                {circles.map(circle => (
-                    <div
-                        key={circle.id}
-                        style={{
+                {
+                    circles.map(e => {
+                        return <div style={{
                             height: "100px",
-                            width: "100px",
-                            borderRadius: "50%",
-                            backgroundColor: circle.isSelected ? "red" : "green",
-                            cursor: 'pointer',
-                            border: '2px solid white',
+                            width: "100px", borderRadius: "50%",
+                            backgroundColor: e.isSelected ? "red" : "green"
                         }}
-                        onClick={() => makeSelected(circle.id)}
-                    />
-                ))}
+                            onClick={() => makeSelected(e.id)}
+                        >
+                        </div>
+                    })
+                }
             </div>
         </>
-    );
+    )
 }
 
 export default Task4;
